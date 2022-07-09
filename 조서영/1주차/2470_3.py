a1404ef8dfbe4e87237c7958796509d0c75ccc33
Choseoyoung <seoyoung0000@naver.com>
2022-07-09 19:24:06 +0900

    edit : 2470 이분탐색을 이용한 풀이 추가(최종)

--- 조서영/1주차/2470_3.py
+++ 조서영/1주차/2470_3.py
@@ -1,7 +1,6 @@
 # 2470, Gold 5
 # https://www.acmicpc.net/problem/2470
-# 5
-# -100 -50 20 40 80
+
 import sys
 
 N=int(sys.stdin.readline())
@@ -9,31 +8,28 @@ S=sorted(list(map(int,sys.stdin.readline().rstrip().split())))
 
 #target값과 가장 가까운 값을 찾는다.
 def search(S, start, end, target):
-    high=end
+    high=end-1
     low=start
     minimum_diff=999999999999999
-    result=-1
-
+    
     while low<high:
         mid=(high+low)//2
         
         diff=target-S[mid]
         if abs(diff)<minimum_diff:
             minimum_diff=abs(diff)
-            result=mid
         if diff==0:
-            high=mid
             break
         elif diff>0: #target>S[mid]
             low=mid+1
         elif diff<0: #target<S[mid]
             high=mid
 
-    return result
+    return high
 
+minimum_diff=999999999999999
 a,b=0,len(S)
 b_tmp=len(S)-1
-minimum_diff2=999999999999999
 for idx in range(len(S)-1):
     a_tmp=idx
     if a_tmp>=b_tmp:
@@ -41,10 +37,9 @@ for idx in range(len(S)-1):
     b_tmp=search(S,idx+1,b_tmp+1, -S[(a_tmp)])
 
     total=S[a_tmp]+S[b_tmp]
-    if abs(total)<minimum_diff2:
-        minimum_diff2=abs(total)
+    if abs(total)<minimum_diff:
+        minimum_diff=abs(total)
         a,b=a_tmp,b_tmp
     
 
 print(S[a],S[b])
-
