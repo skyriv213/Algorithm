5c7b783f7854b628b3c1cce87e11b466364fca9b
Choseoyoung <seoyoung0000@naver.com>
2022-07-29 14:12:46 +0900

    edit : 오답코드 수정

--- 조서영/2주차/16571.py
+++ 조서영/2주차/16571.py
@@ -1,10 +1,9 @@
 # 16571, Gold 3
 # https://www.acmicpc.net/problem/16571
-# col: 열, row=행
+# col: 열, row: 행
 import sys
-from copy import deepcopy
 
-player={"X":1,"O":2}
+player={1:1,-1:2}  #"X":1,"O":2
 board=[]
 X_cnt=0
 O_cnt=0
@@ -17,61 +16,44 @@ for i in range(3):
             O_cnt+=1
         board[i].append(number)
 
-result=[]
-def play_game(board, player_flag, win_flag):
-    ##보드의 모든 곳에 말이 놓여져 있으면 종료, 무승부
-    if "W" in result:
-        return 
-
-    exit_flag=1
+def game(board, player_flag):
+    min_num=1
+    
+    dual_flag=1
     for i in range(3):
         for j in range(3):
             if board[i][j]==0:
-                B=deepcopy(board)
-                B[i][j]=player[player_flag]
-                if check(B, i, j):
-                    if player_flag==win_flag:
-                        result.append("W")
-                        return
+                dual_flag=0
+                board[i][j]=player[player_flag]
+                if check(board, i, j, player[player_flag]):
+                    min_num=min(min_num,-1)
+                min_num=min(min_num, game(board, player_flag*-1)*-1)
+                board[i][j]=0
 
-                if player_flag=="X":
-                    if check_optimal(B, "O")==True:
-                        play_game(B, "O", win_flag)
-                else:
-                    if check_optimal(B, "X")==True:
-                        play_game(B, "X", win_flag)
-   
-    if exit_flag==1:
-        result.append("D")
-      
-def check(B, row, col):
-    if B[row][0]==B[row][1]==B[row][2] and B[row][0]!=0:
+    if dual_flag==1:
+        min_num=min(min_num, 0)
+
+    return min_num       
+
+
+def check(B, row, col, win_flag):
+    if B[row][0]==B[row][1]==B[row][2] and B[row][0]==win_flag:
         return True
-    if B[0][col]==B[1][col]==B[2][col] and B[0][col]!=0:
+    if B[0][col]==B[1][col]==B[2][col] and B[0][col]==win_flag:
         return True
-    if (B[0][0]==B[1][1]==B[2][2] and B[1][1]!=0) or (B[0][2]==B[1][1]==B[2][0] and B[1][1]!=0):
+    if (B[0][0]==B[1][1]==B[2][2] and B[1][1]==win_flag) or (B[0][2]==B[1][1]==B[2][0] and B[1][1]==win_flag):
         return True
     return False
 
-def check_optimal(B, target):
-    for i in range(3):
-        for j in range(3):
-            if board[i][j]==0:
-                B=deepcopy(board)
-                B[i][j]=player[target]
-                if check(B, i, j)==True:
-                    return False    
-    return True
-
 if X_cnt<=O_cnt:
-    play_game(board, "X", "X")
+    result=game(board, 1)
 else:
-    play_game(board, "O", "O")
+    result=game(board, -1)
 
-if "W" in result:
+if result==-1:
     print("W")
-elif "D" in result:
+elif result==0:
     print("D")
-else:
+elif result==1:
     print("L")
-    
+
